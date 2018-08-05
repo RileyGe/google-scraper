6e3b676cfd7b14845ed22f5989df738609619174
Joker <Joker@JokerdeiMac.local>
2018-08-05 23:12:42 +0800

    正则表达式修改完成。

--- sample.php
+++ sample.php
@@ -8,15 +8,21 @@ $page = 1;
 $pageSize = 20;
 $data=file_get_contents('http://www.google.com/search?q='.$keyword.'+site%3Abihu.com&safe=off&ie=utf8&num='.$pageSize.'&start='.($page*$pageSize-$pageSize));
 echo $data;
-preg_match_all('<div\s*class="g">\s*<h3\s*class="r">\s*<a[^<>]*href="[^<>]*?q=([^<>]*)&amp;sa[^<>]*>(.*)</a>\s*</h3>[\s\S]*<span\s*class="st">([\s\S]*)</span>', $data, $matches);
+preg_match_all('@<div\s*class="g">\s*<h3\s*class="r">\s*<a[^<>]*href="[^<>]*?q=(https://[^<>]*bihu.com/[^<>]*)&amp;sa[^<>]*>(.*)</a>\s*</h3>[\s\S]*<span\s*class="st">([\s\S]*)</span>@isU', $data, $matches);
 for ($j = 0; $j < count($matches[1]); $j++) {
     echo $matches[1][$j];
+    echo "</br>";
 }
 for ($j = 0; $j < count($matches[2]); $j++) {
-    echo $matches[1][$j];
+    echo $matches[2][$j];
+    echo "</br>";
 }
+echo "</br>";
+echo "</br>";
+echo "</br>";
 for ($j = 0; $j < count($matches[3]); $j++) {
-    echo $matches[1][$j];
+    echo $matches[3][$j];
+    echo "</br>";
 }
 // $arr=$obj->getUrlList(urlencode('car'),1);
 // foreach($arr as $key=>$value){
