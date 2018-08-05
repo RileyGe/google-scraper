2d0723a67551d4a6f8c903ee16f27355bae520ac
Joker <Joker@JokerdeiMac.local>
2018-08-05 15:36:51 +0800

    测试正则表达式的可用性

--- GoogleScraper.class.php
+++ GoogleScraper.class.php
@@ -9,10 +9,12 @@ class GoogleScraper
 	var $urlList	=	array();
 	var $time1		=	4000000;
 	var $time2		=	8000000;
-	var $proxy		=	"";
 	var $cookie		=	"";
 	var $header		=	"";
 	var $ei			=	"";
+	var $page		=	1;
+	var $pageSize	=	20;
+
 
 
 	function __construct() {
@@ -27,87 +29,93 @@ class GoogleScraper
 
 	function getpagedata($url)
 	{
-		$ch = curl_init();
-		curl_setopt($ch, CURLOPT_URL, $url);
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-		curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
-		curl_setopt($ch, CURLOPT_USERAGENT,'Mozilla/5.0 (Windows NT 5.1; rv:2.0.1) Gecko/20100101 Firefox/4.0.1');
-		curl_setopt($ch, CURLOPT_ENCODING, 'gzip,deflate');
-		curl_setopt($ch, CURLOPT_HTTPHEADER, $this->headers);
-		curl_setopt($ch, CURLOPT_COOKIEFILE,  $this->cookie);
-		curl_setopt($ch, CURLOPT_COOKIEJAR,  $this->cookie);
-		curl_setopt($ch, CURLOPT_PROXY, $this->proxy);
-		curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 30);
-		$data=curl_exec($ch);
-		curl_close($ch);
-		return $data;
+		// $ch = curl_init();
+		// curl_setopt($ch, CURLOPT_URL, $url);
+		// curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		// curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+		// curl_setopt($ch, CURLOPT_USERAGENT,'Mozilla/5.0 (Windows NT 5.1; rv:2.0.1) Gecko/20100101 Firefox/4.0.1');
+		// curl_setopt($ch, CURLOPT_ENCODING, 'gzip,deflate');
+		// curl_setopt($ch, CURLOPT_HTTPHEADER, $this->headers);
+		// curl_setopt($ch, CURLOPT_COOKIEFILE,  $this->cookie);
+		// curl_setopt($ch, CURLOPT_COOKIEJAR,  $this->cookie);
+		// curl_setopt($ch, CURLOPT_PROXY, $this->proxy);
+		// curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 30);
+		// $data=curl_exec($ch);
+		// curl_close($ch);
+
+		return file_get_contents($url);
 	}
 
-	function pause() {
-		usleep(rand($this->time1,$this->time2));
-	}
+	// function pause() {
+	// 	usleep(rand($this->time1,$this->time2));
+	// }
 
-	function initGoogle() {
-		$data=$this->getpagedata('http://www.google.com');		//	Open google.com ( Might redirect to country specific site e.g. www.google.co.in)
-		$this->pause();
-		$this->getpagedata('http://www.google.com/ncr');	//	Moves back to google.com
-	}
+	// function initGoogle() {
+	// 	$data=$this->getpagedata('http://www.google.com');		//	Open google.com ( Might redirect to country specific site e.g. www.google.co.in)
+	// 	$this->pause();
+	// 	$this->getpagedata('http://www.google.com/ncr');	//	Moves back to google.com
+	// }
 
 
 	// This function opens the preference page and saves the count for "Results per page" to 100
-	function setPreference() {
-		$data=$this->getpagedata('http://www.google.com/preferences?hl=en');
-		preg_match('/<input type="hidden" name="sig" value="(.*?)">/', $data, $matches);
-		$this->pause();
-		$this->getpagedata('http://www.google.com/setprefs?sig='.urlencode($matches[1]).'&hl=en&lr=lang_en&safeui=images&suggon=2&newwindow=0&num=100&q=&prev=http%3A%2F%2Fwww.google.com%2F&submit2=Save+Preferences+');
-	}
+	// function setPreference() {
+	// 	$data=$this->getpagedata('http://www.google.com/preferences?hl=en');
+	// 	preg_match('/<input type="hidden" name="sig" value="(.*?)">/', $data, $matches);
+	// 	$this->pause();
+	// 	$this->getpagedata('http://www.google.com/setprefs?sig='.urlencode($matches[1]).'&hl=en&lr=lang_en&safeui=images&suggon=2&newwindow=0&num=100&q=&prev=http%3A%2F%2Fwww.google.com%2F&submit2=Save+Preferences+');
+	// }
 
 	function fetchUrlList()
 	{
-		for($i=0;$i<1001;$i=$i+100) {
-			$data=$this->getpagedata('http://www.google.com/search?q='.$this->keyword.'&num=100&hl=en&biw=1280&bih=612&prmd=ivns&ei='.$this->ei.'&start='.$i.'&sa=N');
-			preg_match('/;ei=(.*?)&amp;/', $data, $matches);
-			if(empty($matches))
-			{
-				preg_match('/;sei=(.*?)"/', $data, $matches);
-				$this->ei=urlencode($matches[1]);
-
-				if(empty($matches))
-				{
-					file_put_contents("data.html",$data);
-					exit();
-				}
-			} else {
-				$this->ei=urlencode($matches[1]);
-			}
-
-			if ($data) {
-				if(preg_match("/sorry.google.com/", $data)) {
-					echo "You are blocked";
-					exit;
-				} else {
-					preg_match_all('@<h3\s*class="r">\s*<a[^<>]*href="[^<>]*?q=([^<>]*)&amp;sa[^<>]*>(.*)</a>\s*</h3>@siU', $data, $matches);
-					for ($j = 0; $j < count($matches[1]); $j++) {
-						array_push($this->urlList, $matches[1][$j]);
-					}
-				}
-			}
-			else
-			{
-				echo "Problem fetching the data";
-				exit;
-			}
-			$this->pause();
+		// for($i=0;$i<1001;$i=$i+100) {
+		$data=$this->getpagedata('http://www.google.com/search?q='.$this->keyword.'+site%3Abihu.com&safe=off&ie=utf8&num='.$this->pageSize.'&start='.($this->page*$this->pageSize-$this->pageSize));
+		preg_match_all('@<h3\s*class="r">\s*<a[^<>]*href="[^<>]*?q=([^<>]*)&amp;sa[^<>]*>(.*)</a>\s*</h3>@siU', $data, $matches);
+		for ($j = 0; $j < count($matches[1]); $j++) {
+			array_push($this->urlList, $matches[1][$j]);
 		}
+
+			// preg_match('/;ei=(.*?)&amp;/', $data, $matches);
+			// if(empty($matches))
+			// {
+			// 	preg_match('/;sei=(.*?)"/', $data, $matches);
+			// 	$this->ei=urlencode($matches[1]);
+
+			// 	if(empty($matches))
+			// 	{
+			// 		file_put_contents("data.html",$data);
+			// 		exit();
+			// 	}
+			// } else {
+			// 	$this->ei=urlencode($matches[1]);
+			// }
+
+			// if ($data) {
+			// 	if(preg_match("/sorry.google.com/", $data)) {
+			// 		echo "You are blocked";
+			// 		exit;
+			// 	} else {
+					
+			// 		for ($j = 0; $j < count($matches[1]); $j++) {
+			// 			array_push($this->urlList, $matches[1][$j]);
+			// 		}
+			// 	}
+			// }
+			// else
+			// {
+			// 	echo "Problem fetching the data";
+			// 	exit;
+			// }
+			// $this->pause();
+		// }
 	}
 
-	function getUrlList($keyword,$proxy='') {
+	function getUrlList($keyword, $pageNo = 1) {
 		$this->keyword=$keyword;
-		$this->proxy=$proxy;
-		$this->initGoogle();
-		$this->pause();
-		$this->setPreference();
-		$this->pause();
+		$this->page = $pageNo;
+		// $this->initGoogle();
+		// $this->pause();
+		// $this->setPreference();
+		// $this->pause();
 		$this->fetchUrlList();
 		return $this->urlList;
 	}
--- sample.php
+++ sample.php
@@ -1,9 +1,26 @@
 <?php
-ini_set ( 'max_execution_time', 0);
-include 'GoogleScraper.class.php';
-$obj=new GoogleScraper();
+// ini_set ( 'max_execution_time', 0);
+// include 'GoogleScraper.class.php';
+// $obj=new GoogleScraper();
 // Pass your keyword and proxy ip here.
-$arr=$obj->getUrlList(urlencode('car'),'');
-print_r($arr);
+$keyword = '圣杯';
+$page = 1;
+$pageSize = 20;
+$data=file_get_contents('http://www.google.com/search?q='.$keyword.'+site%3Abihu.com&safe=off&ie=utf8&num='.$pageSize.'&start='.($page*$pageSize-$pageSize));
+echo $data;
+preg_match_all('<div\s*class="g">\s*<h3\s*class="r">\s*<a[^<>]*href="[^<>]*?q=([^<>]*)&amp;sa[^<>]*>(.*)</a>\s*</h3>\s*<span\s*class="st">(.*)</span>', $data, $matches);
+for ($j = 0; $j < count($matches[1]); $j++) {
+    echo $matches[1][$j];
+}
+for ($j = 0; $j < count($matches[2]); $j++) {
+    echo $matches[1][$j];
+}
+for ($j = 0; $j < count($matches[3]); $j++) {
+    echo $matches[1][$j];
+}
+// $arr=$obj->getUrlList(urlencode('car'),1);
+// foreach($arr as $key=>$value){
+//     echo $value;
+// }
 
 ?>
